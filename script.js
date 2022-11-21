9a8b452dd4991162f7f0888cc55a485d5976c452
blezno <gonzaleznovoa.b@protonmail.com>
2022-11-21 10:55:29 +0100

    first functionnality

---
+++ script.js
@@ -0,0 +1,11 @@
+// Fonctionnalité nº1 : Lorsque l'utilisateur va cliquer sur le footer portant le tag footer, on affichera le mot 'clique' en console. 
+// bonus : utiliser la méthode addEventListenner("click", function(){})
+
+// Fonctionnalité nº1 bis : Upgrade de la première fonctionnalité : lorsque l'utilisateur va cliquer sur le footer, la console va afficher
+
+let foot = document.querySelector('footer');// définit une variable qui va aller chercher tout les tags footer
+foot.addEventListener("click", show); // utilisation du addEventListener qui va venir enclencher l'action click
+    function show(){ //définit la fonction pour afficher en console le "clique"
+    console.log("clique");
+};
+
